f67e8294058f8e0595d5ba036be95f61cef9bc02
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-07-15 19:38:56 +0200

    Initial commit

---
+++ src/main/java/com/example/employeemanagerwithangular/EmployeeManagerWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagerwithangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerWithAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagerWithAngularApplication.class, args);
+    }
+
+}
