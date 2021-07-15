d49b63a193d3b744a736d9ea7eb1205dc31807dd
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-07-15 19:43:12 +0200

    Add Custom Message

--- src/main/java/com/example/employeemanagerwithangular/EmployeeManagerWithAngularApplication.java
+++ src/main/java/com/example/employeemanagerwithangular/EmployeeManagerWithAngularApplication.java
@@ -2,9 +2,20 @@ package com.example.employeemanagerwithangular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 
 @SpringBootApplication
 public class EmployeeManagerWithAngularApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Employee API for ANGULAR ";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeManagerWithAngularApplication.class, args);
