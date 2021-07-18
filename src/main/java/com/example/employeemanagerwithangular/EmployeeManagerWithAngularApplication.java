8af762dc6f878cad749f4b6c2ec1ca896c561fa0
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-07-18 19:41:28 +0200

    Fix packaging

--- src/main/java/com/example/employeemanagerwithangular/EmployeeManagerWithAngularApplication.java
+++ src/main/java/com/example/employeemanagerwithangular/EmployeeManagerWithAngularApplication.java
@@ -33,7 +33,7 @@ public class EmployeeManagerWithAngularApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://boiling-escarpment-06048.herokuapp.com"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
