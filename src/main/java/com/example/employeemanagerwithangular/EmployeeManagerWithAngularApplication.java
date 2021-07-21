544047fee314dc774d8e4c9c72864ff57d080de5
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-07-21 19:13:11 +0200

    Add frontend on heroku

--- src/main/java/com/example/employeemanagerwithangular/EmployeeManagerWithAngularApplication.java
+++ src/main/java/com/example/employeemanagerwithangular/EmployeeManagerWithAngularApplication.java
@@ -34,7 +34,7 @@ public class EmployeeManagerWithAngularApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://angular-employee-frontend.herokuapp.com"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
